0fb4d85eed947d8542861676bcafa9a11e60d7b2
ikerepeks <shakir_1998@yahoo.com.my>
2020-07-11 23:32:43 +0800

    Register and Edit Bank
    Register and Edit Bank features completed except the navigation bar to other pages

--- expenseTrackerClient/src/java/BankController.java
+++ expenseTrackerClient/src/java/BankController.java
@@ -26,7 +26,9 @@ public class BankController {
 
     private String bankname;
     int userid = 1;
-    private ArrayList<String> banklist = new ArrayList<String>();
+    private ArrayList<BankObject> banklist = new ArrayList<>();
+    private String nameholder;
+    private int idholder;
 
     public BankController() {
     }
@@ -45,6 +47,7 @@ public class BankController {
             stmt.executeUpdate();
             con.close();
             return "Insert Succesfully";
+            
 
         } catch (SQLException ex) {
             Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
@@ -60,11 +63,12 @@ public class BankController {
             Connection con = st.getConnection();
 
             st = con.createStatement();
-            String sql = "Select bankname from bank where user_id = " + userid;
+            String sql = "Select id,bankname from bank where user_id = " + userid;
             ResultSet result;
             result = st.executeQuery(sql);
             while (result.next()) {
-                banklist.add(result.getString("bankname"));
+                BankObject holder = new BankObject(result.getInt("id"),result.getString("bankname"));
+                banklist.add(holder);
             }
 
             con.close();
@@ -77,6 +81,41 @@ public class BankController {
         return banklist;
     }
     
+    public String deleteBank(){
+        Statement st;
+        try {
+            st = SQLConnection.getInstance().getSQLConnection();
+            Connection con = st.getConnection();
+            PreparedStatement stmt;
+            stmt = con.prepareStatement("DELETE FROM BANK WHERE ID = "+idholder);
+            stmt.executeUpdate();
+            con.close();
+            return "Delete Success!";
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+            return "Delete Failed!";
+        }
+    }
+    
+    public String editBank(){
+        Statement st;
+        try {
+            st = SQLConnection.getInstance().getSQLConnection();
+            Connection con = st.getConnection();
+            PreparedStatement stmt;
+            stmt = con.prepareStatement("UPDATE BANK SET BANKNAME = '"+nameholder+"' WHERE ID = " + idholder);
+            stmt.executeUpdate();
+            con.close();
+            return "Edit Success!";
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+            return "Edit Failed!";
+        }
+    }
+    
+    
     //setter and getter
     public String getBankname() {
         return bankname;
@@ -86,4 +125,21 @@ public class BankController {
         this.bankname = bankname;
     }
 
+    public String getNameholder() {
+        return nameholder;
+    }
+
+    public void setNameholder(String nameholder) {
+        this.nameholder = nameholder;
+    }
+
+    public int getIdholder() {
+        return idholder;
+    }
+
+    public void setIdholder(int idholder) {
+        this.idholder = idholder;
+    }
+    
+    
 }
